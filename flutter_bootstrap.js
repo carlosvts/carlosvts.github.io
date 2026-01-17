fa7d810c430a1174c4babd9ea7cf742b30c6c22e
carlosvts <carlosvtsdev@gmail.com>
2026-01-17 10:03:21 +0000

    Update from https://github.com/carlosvts/portfolio/commit/573b37d195f6984ad4f350195769268660d1a048

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "238703102"
+    serviceWorkerVersion: "291731051"
   }
 });
